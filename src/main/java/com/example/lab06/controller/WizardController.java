f84f2ef6d22e2e1ecb0853bb41ed1ccaa036a9e1
kapraoKai <wavezaza2@outlook.com>
2022-10-04 12:17:36 +0700

    Initial commit

---
+++ src/main/java/com/example/lab06/controller/WizardController.java
@@ -0,0 +1,76 @@
+package com.example.lab06.controller;
+
+import com.example.lab06.pojo.Wizard;
+import com.example.lab06.pojo.Wizards;
+import com.example.lab06.repository.WizardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class WizardController {
+
+    @Autowired
+    private WizardService service;
+
+
+
+    @RequestMapping(value = "/wizards", method = RequestMethod.GET)
+    public ResponseEntity<?> getWizards() {
+        List<Wizard> wizards = service.retrieveWizards();
+        return ResponseEntity.ok(wizards);
+    }
+    @RequestMapping(value ="/addWizard/{name}/{sex}/{money}/{school}/{house}/{position}", method = RequestMethod.POST)
+    public ResponseEntity<?> createWizard(@PathVariable("name") String name,
+                                          @PathVariable("sex") String sex,
+                                          @PathVariable("money") String money,
+                                          @PathVariable("school") String school,
+                                          @PathVariable("house") String house,
+                                          @PathVariable("position") String position){
+        String s = "";
+        if(sex.equals("Male")){
+            s = "m";
+        }else {
+            s = "f";
+        }
+        Wizard n = service.createWizard(new Wizard(null, s, name, school, house, money, position));
+        return ResponseEntity.ok(n);
+    }
+
+    @RequestMapping(value ="/deleteWizard/{name}", method = RequestMethod.POST)
+    public boolean deleteBook(@PathVariable("name") String name){
+        Wizard wizard = service.retrieveWizardsByName(name);
+        boolean status = service.deleteWizard(wizard);
+        return status;
+    }
+
+    @RequestMapping(value ="/updateWizard/{nameOld}/{nameNew}/{sex}/{money}/{school}/{house}/{position}", method = RequestMethod.POST)
+    public boolean updateWizard(@PathVariable("nameOld") String nameOld,
+                                @PathVariable("nameNew") String nameNew,
+                                @PathVariable("sex") String sex,
+                                @PathVariable("money") String money,
+                                @PathVariable("school") String school,
+                                @PathVariable("house") String house,
+                                @PathVariable("position") String position){
+        Wizard wizardold = service.retrieveWizardsByName(nameOld);
+        String s = "";
+        if(sex.equals("Male")){
+            s = "m";
+        }else {
+            s = "f";
+        }
+        Wizard wizard = service.updateWizard(new Wizard(wizardold.get_id(), s, nameNew, school, house, money, position));
+        if(wizard != null){
+
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/com/example/lab06/view/MainWizardView.java
@@ -0,0 +1,187 @@
+package com.example.lab06.view;
+
+import com.example.lab06.pojo.Wizard;
+import com.example.lab06.pojo.Wizards;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
+import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+import static com.vaadin.flow.component.notification.Notification.Position.TOP_CENTER;
+
+@Route(value = "mainPage.it")
+public class MainWizardView extends VerticalLayout {
+    private TextField name;
+    private RadioButtonGroup gender;
+    private ComboBox position, school, house;
+    private NumberField dollar;
+    private Button previous, create, update, delete, next;
+    private Notification nf;
+    private int index = -1;
+    private String oldName = "";
+    public MainWizardView() {
+        name = new TextField();
+        name.setPlaceholder("Fullname");
+
+        gender = new RadioButtonGroup<>();
+        gender.setLabel("Gender: ");
+        gender.setItems("Male", "Female");
+        gender.setValue("Male");
+
+        position = new ComboBox<>();
+        position.setItems("Student", "Teacher");
+        position.setPlaceholder("Position");
+
+        school = new ComboBox<>();
+        school.setItems("Hogwarts", "Beauxbatons", "Durmstrang");
+        school.setPlaceholder("School");
+
+        house = new ComboBox<>();
+        house.setItems("Gryffindor", "Ravenclaw", "Hufflepuff", "Slyther");
+        house.setPlaceholder("House");
+
+        dollar = new NumberField();
+        dollar.setPlaceholder("Dollars");
+        dollar.setPrefixComponent(new Span("$"));
+
+        previous = new Button("<<");
+        create = new Button("Create");
+        update = new Button("Update");
+        delete = new Button("Delete");
+        next = new Button(">>");
+
+        nf = new Notification();
+
+        HorizontalLayout hbtn = new HorizontalLayout();
+        hbtn.add(previous, create, update, delete, next);
+        add(name, gender, position, dollar, school, house, hbtn);
+
+        next.addClickListener(event -> {
+            List<Wizard> out = WebClient.create()
+                    .get()
+                    .uri("http://localhost:8080/wizards")
+                    .retrieve()
+                    .bodyToMono(List.class)
+                    .block();
+            if(index == out.size()-1){
+                index = 0;
+            }else{
+                index += 1;
+            }
+            ObjectMapper mapper = new ObjectMapper();
+            Wizard wizard = mapper.convertValue(out.get(index), Wizard.class);
+            oldName = wizard.getName();
+            name.setValue(wizard.getName());
+            if(wizard.getSex().equals("m")){
+                gender.setValue("Male");
+            }else {
+                gender.setValue("Female");
+            }
+            position.setValue(wizard.getPosition());
+            dollar.setValue(Double.valueOf(wizard.getMoney()));
+            school.setValue(wizard.getSchool());
+            house.setValue(wizard.getHouse());
+        });
+
+        previous.addClickListener(event -> {
+            List<Wizard> out = WebClient.create()
+                    .get()
+                    .uri("http://localhost:8080/wizards")
+                    .retrieve()
+                    .bodyToMono(List.class)
+                    .block();
+            if(index <= 0){
+                index = out.size()-1;
+            }else{
+                index -= 1;
+            }
+            ObjectMapper mapper = new ObjectMapper();
+            Wizard wizard = mapper.convertValue(out.get(index), Wizard.class);
+            oldName = wizard.getName();
+            name.setValue(wizard.getName());
+            if(wizard.getSex().equals("m")){
+                gender.setValue("Male");
+            }else {
+                gender.setValue("Female");
+            }
+            position.setValue(wizard.getPosition());
+            dollar.setValue(Double.valueOf(wizard.getMoney()));
+            school.setValue(wizard.getSchool());
+            house.setValue(wizard.getHouse());
+        });
+
+        create.addClickListener(event -> {
+            String newName = name.getValue();
+            String newGender = gender.getValue()+"";
+            String newPosition = position.getValue()+"";
+            String newDollars = dollar.getValue()+"";
+            String newSchool = school.getValue()+"";
+            String newHouse = house.getValue()+"";
+
+
+            Wizard result = WebClient.create()
+                    .post()
+                    .uri("http://localhost:8080/addWizard/"+newName+"/"+newGender+"/"+newDollars+"/"+newSchool+"/"+newHouse+"/"+newPosition)
+                    .retrieve()
+                    .bodyToMono(Wizard.class)
+                    .block();
+            if(result != null){
+                nf = Notification.show("Wizard has been created", 3000, TOP_CENTER);
+            } else {
+                nf = Notification.show("Can't create Wizard", 3000, TOP_CENTER);
+            }
+        });
+
+        update.addClickListener(event -> {
+            String newName = name.getValue();
+            String newGender = gender.getValue()+"";
+            String newPosition = position.getValue()+"";
+            String newDollars = dollar.getValue()+"";
+            String newSchool = school.getValue()+"";
+            String newHouse = house.getValue()+"";
+            boolean success = WebClient.create()
+                    .post()
+                    .uri("http://localhost:8080/updateWizard/"+oldName+"/"+newName+"/"+newGender+"/"+newDollars+"/"+newSchool+"/"+newHouse+"/"+newPosition)
+                    .retrieve()
+                    .bodyToMono(boolean.class)
+                    .block();
+            if(success){
+                nf = Notification.show("Wizard has been updated", 3000, TOP_CENTER);
+            } else {
+                nf = Notification.show("Can't update Wizard", 3000, TOP_CENTER);
+            }
+        });
+
+        delete.addClickListener(event -> {
+            String newName = name.getValue();
+            boolean success = WebClient.create()
+                    .post()
+                    .uri("http://localhost:8080/deleteWizard/"+newName)
+                    .retrieve()
+                    .bodyToMono(boolean.class)
+                    .block();
+            if(success){
+                nf = Notification.show("Wizard has been deleted", 3000, TOP_CENTER);
+                this.name.setValue("");
+                this.gender.setValue("");
+                this.position.setValue("");
+                this.dollar.setValue(0.00);
+                this.house.setValue("");
+                this.school.setValue("");
+            } else {
+                nf = Notification.show("Can't delete Wizard", 3000, TOP_CENTER);
+            }
+        });
+
+    }
+}
